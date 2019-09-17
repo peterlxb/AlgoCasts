278000f701de93f4e646089acc0c293345c28db5
peter liu <peterlxb@gmail.com>
2019-09-17 21:40:16 +0800

    linked-list/swapPairs

--- linked-list/tasks/reverseList.py
+++ linked-list/tasks/reverseList.py
@@ -16,8 +16,9 @@ class Solution(object):
             保存 head 和 head.next 的引用
             """
             cur = head
+            # 头结点每次指向下一个结点
             head = head.next
             cur.next = prev
-            "最后要移动prev"
+            "最后要移动prev 指向上一次的头结点，也就是第一行的cur = head"
             prev = cur
         return prev
