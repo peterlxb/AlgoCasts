cef1eadf2b321fe18bbfa181141537bbf80bc390
peter liu <peterlxb@gmail.com>
2019-10-17 13:14:14 +0800

    heap/KthLargest

--- Heap/tasks/KthLargest.py
+++ Heap/tasks/KthLargest.py
@@ -1,3 +1,14 @@
+"""
+int k = 3;
+int[] arr = [4,5,8,2];
+KthLargest kthLargest = new KthLargest(3, arr);
+kthLargest.add(3);   // returns 4 第3大元素
+kthLargest.add(5);   // returns 5
+kthLargest.add(10);  // returns 5
+kthLargest.add(9);   // returns 8
+kthLargest.add(4);   // returns 8
+"""
+
 class KthLargest(object):
     def __init__(self, k, nums):
         self.pool = nums
@@ -5,8 +16,7 @@ class KthLargest(object):
         heapq.heapify(self.pool)
         while len(self.pool) > k:
             heapq.heappop(self.pool)
-
-            
+        
     def add(self, val):
         if len(self.pool) < self.k:
             heapq.heappush(self.pool, val)
