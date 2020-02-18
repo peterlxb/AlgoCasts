97f81358d010a50f78155e1afbd88728fa727a59
刘晓波 <peterlxb@gmail.com>
2020-02-18 22:44:39 +0800

    linkedlist/swappairs

--- linked-list/tasks/swapPairs.java
+++ linked-list/tasks/swapPairs.java
@@ -1,5 +1,8 @@
 /**
  * Definition for singly-linked list.
+
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ 
  * public class ListNode {
  *     int val;
  *     ListNode next;
@@ -12,11 +15,15 @@ public class Solution {
         ListNode cur = head;
         ListNode newHead = head.next;
         while (cur != null && cur.next != null) {
-            ListNode tmp = cur;
+            // 用一个临时变量存储当前节点
+            ListNode tmp = cur; 
             cur = cur.next;
-            tmp.next = cur.next;
+            // 等价于下一个循环开始的节点 2n + 1 curr.next.next
+            tmp.next = cur.next; 
             cur.next = tmp;
+            // 下一次开始的 curr 节点
             cur = tmp.next;
+            // 处理边界条件 基数或偶数
             if (cur != null && cur.next != null) tmp.next = cur.next;
         }
         return newHead;
